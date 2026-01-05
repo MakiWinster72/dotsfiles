56c59630472777c60e821a236ad8d45964fd60b6
MakiWinster <makiiiiiiiho@gmail.com>
2026-01-06 06:34:52 +0800

    nvim dashboard

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,5 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   opts = {
     github = {
       download_url_template = "https://mirror.ghproxy.com/https://github.com/%s/releases/download/%s/%s",
