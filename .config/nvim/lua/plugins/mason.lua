1bc16bb06da2e7d58c6ab906a69af75cdad0f7cf
MakiWinster <makiiiiiiiho@gmail.com>
2026-01-27 20:41:13 +0800

    mason rm proxy

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -2,7 +2,7 @@ return {
   "mason-org/mason.nvim",
   opts = {
     github = {
-      download_url_template = "https://mirror.ghproxy.com/https://github.com/%s/releases/download/%s/%s",
+      download_url_template = "https://github.com/%s/releases/download/%s/%s",
     },
   },
 }
